d86d1018058a68ade69a334571c142240d820175
Kimmy Costa <k.costa@eurosmallbiz.com>
2023-06-24 22:46:03 +0200

    slicers

--- BoreholeVisualisation/assets/dataVisuCompute.py
+++ BoreholeVisualisation/assets/dataVisuCompute.py
@@ -1,19 +1,137 @@
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import plotly.express as px
 
 import numpy as np
-X, Y, Z = np.mgrid[-1:1:30j, -1:1:30j, -1:1:30j]
-values =    np.cos(np.pi*X) * np.cos(np.pi*Z) * np.sin(np.pi*Y)
 
-fig = go.Figure(data=go.Volume(
+X, Y, Z = np.mgrid[-1:1:30j, -1:1:30j, -1:1:30j]
+#values = np.cos(np.pi * X) * np.cos(np.pi * Z) * np.sin(np.pi * Y)
+
+values = np.load("realizations.npy")[0]
+nb_frames = volume.shape[2]
+
+
+fig = go.Figure(data=go.Isosurface(
+    x=X.flatten(),
+    y=Y.flatten(),
+    z=Z.flatten(),
+    value=values.flatten(),
+    isomin=1,
+    isomax=12,
+    opacity=0.1,  # needs to be small to see through all surfaces
+    #surface_count=21,  # needs to be a large number for good volume rendering
+    caps=dict(x_show=False, y_show=False),
+    slices_z=dict(show=True, locations=[k,k]),
+    ))
+
+
+sliders = [
+            {
+                "pad": {"b": 10, "t": 60},
+                "len": 0.9,
+                "x": 0.1,
+                "y": 0,
+                "steps": [
+                    {
+                        "args": [[f.name], frame_args(0)],
+                        "label": str(k),
+                        "method": "animate",
+                    }
+                    for k, f in enumerate(fig.frames)
+                ],
+            }
+        ]
+
+fig.update_layout(
+         updatemenus = [
+            {
+                "buttons": [
+                    {
+                        "args": [None, frame_args(50)],
+                        "label": "&#9654;", # play symbol
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], frame_args(0)],
+                        "label": "&#9724;", # pause symbol
+                        "method": "animate",
+                    },
+                ],
+                "direction": "left",
+                "pad": {"r": 10, "t": 70},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }
+         ],
+         sliders=sliders
+)
+
+# Initialize figure with 2 3D subplots
+#fig = make_subplots(
+#    rows=1, cols=3,
+#    specs=[[{'type': 'volume'}, {'type': 'volume'}, {'type': 'volume'}]]
+#)
+
+#
+# Generate data (dummy 3D volume)
+#
+volume_dummy_full = go.Volume(
+    x = values[0],
+    y = values[1],
+    z = values[2],
+    value=values.flatten(),
+    cmin=-1,
+    cmax=13,
+    opacity=0.1,  # needs to be small to see through all surfaces
+)
+
+volume_dummy_sliced_Z = go.Volume(
     x=X.flatten(),
     y=Y.flatten(),
     z=Z.flatten(),
     value=values.flatten(),
     isomin=-0.1,
     isomax=0.8,
-    opacity=1.0, # needs to be small to see through all surfaces
-    surface_count=21, # needs to be a large number for good volume rendering
-    ))
+    opacity=0.5,  # needs to be small to see through all surfaces
+    surface_count=21,  # needs to be a large number for good volume rendering
+    slices_z=dict(show=True, locations=[0.4]),
+    surface=dict(fill=0.1, pattern='odd'),
+    caps=dict(x_show=False, y_show=False, z_show=False),  # no caps
+)
 
+volume_dummy_sliced_X = go.Volume(
+    x=X.flatten(),
+    y=Y.flatten(),
+    z=Z.flatten(),
+    value=values.flatten(),
+    isomin=-0.1,
+    isomax=0.8,
+    opacity=0.5,  # needs to be small to see through all surfaces
+    surface_count=21,  # needs to be a large number for good volume rendering
+    slices_x=dict(show=True, locations=[0.4]),
+    surface=dict(fill=0.1, pattern='odd'),
+    caps=dict(x_show=False, y_show=False, z_show=False),  # no caps
+)
+
+#
+# Add volume/surfaces to subplots
+#
+#fig.add_trace(
+#    volume_dummy_full,
+#    #row=1, col=1
+#)
+#fig.add_trace(
+#    volume_dummy_sliced_Z,
+#    row=1, col=2
+#)
+#fig.add_trace(
+#    volume_dummy_sliced_X,
+#    row=1, col=3
+#)
+
+# Event Handlers
+
+
+# Write to HTML
 fig.write_html("dataVisualization.html")
---
+++ BoreholeVisualisation/assets/dataVisuSliceCompute.py
@@ -0,0 +1,204 @@
+# Import data
+import time
+import numpy as np
+import plotly.io as io
+
+# Define frames
+import plotly.graph_objects as go
+
+### read the volume
+volume = np.load("realizations.npy")[0]
+#### set the color scale
+colorscales = [[0,'white'],[0.1, 'red'], [0.2,'blue'],[0.3,'green'],[0.4,'darkgoldenrod'], [0.5, 'lightgreen'], [0.6,'yellow'],[0.7,'black']]
+
+####### creating slices for z axis
+
+nb_frames0 = volume.shape[2]
+
+fig0 = go.Figure(frames=[go.Frame(data=go.Surface(
+    z=(k) * np.ones(volume[:,:,k].shape),
+    surfacecolor=volume[:,:,k],
+    cmin=1, cmax=13,
+    colorscale=colorscales,
+    opacityscale=[[0, 0], [1/13, 1], [1, 1]]),
+    name=str(k) # you need to name the frame for the animation to behave properly
+    )
+    for k in range(nb_frames0)])
+
+# Add data to be displayed before animation starts
+fig0.add_trace(go.Surface(
+    z=0 * np.ones(volume[:,:,0].shape),
+    surfacecolor=volume[:,:,0],
+    colorscale=colorscales,
+    cmin=1, cmax=13,
+    #colorbar=dict(thickness=20, ticklen=4)
+    ))
+
+def frame_args(duration):
+    return {
+            "frame": {"duration": duration},
+            "mode": "immediate",
+            "fromcurrent": True,
+            "transition": {"duration": duration, "easing": "linear"},
+        }
+#create slider
+sliders = [
+            {
+                "pad": {"b": 10, "t": 20},
+                "len": 0.9,
+                "x": 0.1,
+                "y": 0,
+                "currentvalue": {
+                        "offset": 20,
+                        "xanchor": "center",
+                        "font": {
+                          "color": '#888',
+                          "size": 15
+                        }
+                      },
+                "font": {"color": 'white'},
+                "steps": [
+                    {
+                        "args": [[f.name], frame_args(0)],
+                        "label": str(k),
+                        "method": "animate",
+                    }
+                    for k, f in enumerate(fig0.frames)
+                ],
+            }
+        ]
+
+# Layout
+fig0.update_layout(
+         title="slice z",
+         scene = dict(
+            aspectratio=dict(x=1, y=1, z=1),
+            xaxis = dict(visible=False),
+            yaxis = dict(visible=False),
+            zaxis=dict(visible=False),
+            camera = dict(
+                eye=dict(x=0, y=0.5, z=2.0)
+            )
+         ),
+         updatemenus = [
+            {
+                "buttons": [
+                    {
+                        "args": [None, frame_args(50)],
+                        "label": "&#9654;", # play symbol
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], frame_args(0)],
+                        "label": "&#9724;", # pause symbol
+                        "method": "animate",
+                    },
+                ],
+                "direction": "left",
+                "pad": {"r": 10, "t": 70},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }
+         ],
+         sliders=sliders
+)
+
+####### creating slice for y axis
+
+nb_frames = volume.shape[1]
+
+fig = go.Figure(frames=[go.Frame(data=go.Surface(
+
+    z=(k) * np.ones(volume[:,k,:].shape),
+    surfacecolor=volume[:,k,:],
+    cmin=1, cmax=13,
+    colorscale=colorscales,
+    opacityscale=[[0, 0], [1/13, 1], [1, 1]]),
+    name=str(k) # you need to name the frame for the animation to behave properly
+    )
+    for k in range(nb_frames)])
+
+# Add data to be displayed before animation starts
+fig.add_trace(go.Surface(
+    z=0 * np.ones(volume[:,0,:].shape),
+    surfacecolor=volume[:,0,:],
+    colorscale=colorscales,
+    cmin=1, cmax=13,
+    #colorbar=dict(thickness=20, ticklen=4)
+    ))
+
+def frame_args(duration):
+    return {
+            "frame": {"duration": duration},
+            "mode": "immediate",
+            "fromcurrent": True,
+            "transition": {"duration": duration, "easing": "linear"},
+        }
+# create slider
+sliders = [
+            {
+                "pad": {"b": 10, "t": 20},
+                "len": 1.0,
+                "x": 0.1,
+                "y": 0,
+                "currentvalue": {
+                        "offset": 20,
+                        "xanchor": "center",
+                        "font": {
+                          "color": '#888',
+                          "size": 15
+                        }
+                      },
+                "font": {"color": 'white'},
+                "steps": [
+                    {
+                        "args": [[f.name], frame_args(0)],
+                        "label": str(k),
+                        "method": "animate",
+                    }
+                    for k, f in enumerate(fig.frames)
+                ],
+            }
+        ]
+
+# Layout
+fig.update_layout(
+         title="slice y",
+         scene = dict(
+            aspectratio=dict(x=1, y=1, z=1),
+            xaxis = dict(visible=False),
+            yaxis = dict(visible=False),
+            zaxis=dict(visible=False),
+            camera = dict(
+                eye=dict(x=0, y=0.5, z=2.0)
+            )
+         ),
+         updatemenus = [
+            {
+                "buttons": [
+                    {
+                        "args": [None, frame_args(50)],
+                        "label": "&#9654;", # play symbol
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], frame_args(0)],
+                        "label": "&#9724;", # pause symbol
+                        "method": "animate",
+                    },
+                ],
+                "direction": "left",
+                "pad": {"r": 10, "t": 70},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }
+         ],
+         sliders=sliders
+)
+
+# store in the html 2 separated figures in a row (not as subplot)
+with open('dataVisualization.html', 'w', encoding="utf-8") as f:
+    f.writelines(io.to_html(fig0, include_plotlyjs='cnd', full_html=True))
+    f.writelines(io.to_html(fig, include_plotlyjs='cnd', full_html=True))
